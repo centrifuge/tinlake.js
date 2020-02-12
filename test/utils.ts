069be3daf545e82810e5bda2930e51ac7d54178f
ilinzweilin <alina@centrifuge.io>
2020-02-12 13:40:25 +0100

    chore remove conflicts

--- test/utils.ts
+++ test/utils.ts
@@ -1,14 +1,9 @@
 import { Account } from "./types";
-<<<<<<< HEAD
 import { ethI, EthConfig, ContractAbis, ContractAddresses } from '../src/types';
-=======
-import { ethI } from '../src/types';
->>>>>>> upstream/naxos
 const Eth = require('ethjs');
 const SignerProvider = require('ethjs-provider-signer');
 const { sign } = require('ethjs-signer');
 import { waitAndReturnEvents, executeAndRetry } from '../src/ethereum';
-<<<<<<< HEAD
 
 
 export class TestProvider {
@@ -57,45 +52,6 @@ export class TestProvider {
 
   async  relyAccount(usr: Account, contract: string) {;
   }
-=======
-import currencyContractAbi from '../src/abi/test/SimpleToken.abi.json';
-
-// TODO: use strict typing
-
-let eth : ethI;
-let currencyContract: any;
-
-export function initTestUtils(testConfig: any) {
-  const { rpcUrl, godAccount, contractAddresses } = testConfig;
-
-  eth = eth || new Eth(createSignerProvider(rpcUrl, godAccount));
-  const currencyAddress = testConfig.contractAddresses['TINLAKE_CURRENCY'];
-  currencyContract = eth.contract(currencyContractAbi).at(contractAddresses[currencyAddress]);
-}
-
-
-export async function fundAccountWithETH(usr: Account, testConfig: any) {
-  const { godAccount,transactionTimeout, gasLimit } = testConfig;
-  const ethConfig = { from: godAccount.address, gasLimit: `0x${gasLimit.toString(16)}` };
-  const txHash = await executeAndRetry(eth.send, [usr.address, 999, ethConfig]);
-  console.log(`[Currency.mint] txHash: ${txHash}`);
-  await waitAndReturnEvents(eth, txHash, currencyContractAbi, transactionTimeout);
-  console.log("worked worked");
-  // transfer from god Account to provided user Adress
-}
-
-export async function fundAccountWithCurrency(usr: Account, testConfig: any) {
-  const { godAccount,transactionTimeout, gasLimit } = testConfig;
-  const ethConfig = { from: godAccount.address, gasLimit: `0x${gasLimit.toString(16)}` };
-  const txHash = await executeAndRetry(currencyContract.transfer, [godAccount.address, usr.address, 999, ethConfig]);
-  console.log(`[Currency.mint] txHash: ${txHash}`);
-  await waitAndReturnEvents(eth, txHash, currencyContractAbi, transactionTimeout);
-  console.log("worked worked");
-  // transfer from god Account to provided user Adress
-}
-
-export function relyAccount(usr: Account, testConfig: any) {
->>>>>>> upstream/naxos
 }
 
 export function createTinlake(usr: Account, TinlakeSetup: any, testConfig: any) {
@@ -121,10 +77,6 @@ export function createTinlake(usr: Account, TinlakeSetup: any, testConfig: any) 
 }
 
 function createSignerProvider(rpcUrl: string, usr: Account) {
-<<<<<<< HEAD
-=======
-  console.log("god account", usr.address);
->>>>>>> upstream/naxos
     return new SignerProvider(rpcUrl, {
         signTransaction: (rawTx: any, cb: (arg0: null, arg1: any) => void) =>
           cb(null, sign(rawTx, usr.privateKey)),
