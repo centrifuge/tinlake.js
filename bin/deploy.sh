30028d919dea7b99c198b7be5fb98da4d156a410
Charly <charly@centrifuge.io>
2020-02-24 18:47:35 +0100

    first

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,10 +3,11 @@
 
 # remove tinlake submodule and install newest dependency
 [ -d ./tinlake ] && rm -r ./tinlake
-git submodule update --init --recursive 
-git submodule update --recursive --remote --merge 
+[ -d ./tinlake-proxy ] && rm -r ./tinlake-proxy
+git submodule update --init --recursive
+git submodule update --recursive --remote --merge
 
-# superpower user for tinlake.js tests 
+# superpower user for tinlake.js tests
 GOD_ADDRESS=0xf6fa8a3f3199cdd85749ec749fb8f9c2551f9928
 
 # make superpower user from tinlake.js tests to be the governance address (control over the root contract)
@@ -16,7 +17,7 @@ export GOVERNANCE=$GOD_ADDRESS
 # setup local config
 ./tinlake/bin/test/setup_local_config.sh
 
-# create address folder
+ create address folder
 mkdir ./tinlake/deployments
 
 # deploy contracts
@@ -27,15 +28,30 @@ mkdir ./tinlake/deployments
 
 # copy the addresses of deployed contracts from tinlake folder to tinlake.js test folder
 touch ./test/addresses.json
-cat ./tinlake/deployments/addresses_unknown.json > ./test/addresses.json 
+cat ./tinlake/deployments/addresses_unknown.json > ./test/addresses.json
 
-# src env for contract deployment
+## src env for contract deployment
+source ./tinlake/bin/util/util.sh
 source ./tinlake/bin/test/local_env.sh
 
-# rely superpower user on nft collateral contract 
-NFT_COLLATERAL_ADDRESS=$(cat ./test/addresses.json | jq '.COLLATERAL_NFT' | tr -d '"') 
+# rely superpower user on nft collateral contract
+NFT_COLLATERAL_ADDRESS=$(cat ./test/addresses.json | jq '.COLLATERAL_NFT' | tr -d '"')
 seth send $NFT_COLLATERAL_ADDRESS 'rely(address)' $GOD_ADDRESS
 
 # send funds to superpower user
 seth send --value 10000000000000000000000000000000000000000000000000000000 $GOD_ADDRESS
 
+# deploy proxy registry contract
+# build contracts if needed
+cd tinlake-proxy && dapp build --extract
+
+export PROXY_REGISTRY=$(seth send --create ./out/ProxyRegistry.bin 'ProxyRegistry()')
+message Proxy Registry Address: $PROXY_REGISTRY
+
+DEPLOYMENT_FILE=../tinlake/deployments/addresses_$(seth chain).json
+
+addValuesToFile $DEPLOYMENT_FILE <<EOF
+{
+    "PROXY_REGISTRY" :"$PROXY_REGISTRY"
+}
+EOF
